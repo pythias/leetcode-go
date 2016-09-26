e78bd13cdc17925fd6d6944351fa72e81fa4b096
pythias <pythias@gmail.com>
2016-09-26 21:55:56 +0800

    remove comment lines

--- reverse_integer.go
+++ reverse_integer.go
@@ -31,13 +31,3 @@ func reverse(x int) int {
 
     return int(num)
 }
-
-// uint8       the set of all unsigned  8-bit integers (0 to 255)
-// uint16      the set of all unsigned 16-bit integers (0 to 65535)
-// uint32      the set of all unsigned 32-bit integers (0 to 4294967295)
-// uint64      the set of all unsigned 64-bit integers (0 to 18446744073709551615)
-
-// int8        the set of all signed  8-bit integers (-128 to 127)
-// int16       the set of all signed 16-bit integers (-32768 to 32767)
-// int32       the set of all signed 32-bit integers (-2147483648 to 2147483647)
-// int64       the set of all signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
